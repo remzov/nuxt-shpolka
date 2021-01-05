16169b61755753b3a9532af8db9a306da1747bb6
remzov <remzovnik@gmail.com>
2021-01-05 15:37:51 +0300

    404 added

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  <div>
+    h1(v-if="error.statusCode === 404") Страница не найдена!
+    nuxtLink(to="/") На главную
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'basic'
+  }
+</script>
